867e673154be5155f4867e1c72c5251c21ff592a
Jeremy Downes <jeremydownes@hotmail.com>
2020-10-09 13:49:44 -0400

    Updates

---
+++ precache-manifest.65738bde6c3e4684f7cec70219d5d1e2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb006270ce7eb04facb2cb81e42f1d9",
+    "url": "/North_Pole_Meltdown/index.html"
+  },
+  {
+    "revision": "de31334d428a9bf79244",
+    "url": "/North_Pole_Meltdown/static/css/main.e2767a44.chunk.css"
+  },
+  {
+    "revision": "c0a272062c98ed91a262",
+    "url": "/North_Pole_Meltdown/static/js/2.c2887771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/North_Pole_Meltdown/static/js/2.c2887771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de31334d428a9bf79244",
+    "url": "/North_Pole_Meltdown/static/js/main.83bc8860.chunk.js"
+  },
+  {
+    "revision": "9e12e5f3b56e6061fc36",
+    "url": "/North_Pole_Meltdown/static/js/runtime-main.4d92f503.js"
+  },
+  {
+    "revision": "7829b51445a5e5a339ef21a823781b4b",
+    "url": "/North_Pole_Meltdown/static/media/Asenora.7829b514.gif"
+  },
+  {
+    "revision": "ae04822d63b8244ee5627237bf8edb6f",
+    "url": "/North_Pole_Meltdown/static/media/beginning-ending-background.ae04822d.gif"
+  },
+  {
+    "revision": "e7f2f8716c6c835c643d75ff09813007",
+    "url": "/North_Pole_Meltdown/static/media/cactus.e7f2f871.png"
+  },
+  {
+    "revision": "16f240ee6b587875d560e3e6d767cfc5",
+    "url": "/North_Pole_Meltdown/static/media/christmas-lights-border.16f240ee.gif"
+  },
+  {
+    "revision": "e51651eecab8753163fbf11b332427cd",
+    "url": "/North_Pole_Meltdown/static/media/cop.e51651ee.gif"
+  },
+  {
+    "revision": "b22879c6252d33e8ab3800188adb242f",
+    "url": "/North_Pole_Meltdown/static/media/cowboy.b22879c6.gif"
+  },
+  {
+    "revision": "4595abe7037d7952f2e2c0d8bd6e43f7",
+    "url": "/North_Pole_Meltdown/static/media/flag.4595abe7.jpg"
+  },
+  {
+    "revision": "fc9398245c6e5c3cff76456524ac3c42",
+    "url": "/North_Pole_Meltdown/static/media/flames.fc939824.gif"
+  },
+  {
+    "revision": "f104cd5d6b8d1abb00d0a5c51c9488a1",
+    "url": "/North_Pole_Meltdown/static/media/scroll.f104cd5d.png"
+  },
+  {
+    "revision": "9b20520b7076ef24075d5c817c4cbb5d",
+    "url": "/North_Pole_Meltdown/static/media/soldier.9b20520b.gif"
+  },
+  {
+    "revision": "0aac32d8be67c85f0c40595853d58cea",
+    "url": "/North_Pole_Meltdown/static/media/taco.0aac32d8.png"
+  },
+  {
+    "revision": "b3714ee5b180e755c4fb1f552d73734e",
+    "url": "/North_Pole_Meltdown/static/media/tree.b3714ee5.gif"
+  },
+  {
+    "revision": "4779d769c3545692c411e052da246635",
+    "url": "/North_Pole_Meltdown/static/media/trump.4779d769.gif"
+  },
+  {
+    "revision": "64d0226605028c984ce4ff5885a029c3",
+    "url": "/North_Pole_Meltdown/static/media/wall.64d02266.png"
+  }
+]);
